b68a6d862899971387bb572a6af8a343bfd9c6ec
Aayush kumar <rajaayush42@gmail.com>
2026-09-08 11:24:58 +0530

    config: add safe Hardhat network configuration

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+    localhost: { url: "http://127.0.0.1:8545" },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : []
+    }
+  }
+};
